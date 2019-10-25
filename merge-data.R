a2feb86c0f0e90692d3c767e2042f209d2cd9227
liampshaw <liam.philip.shaw@gmail.com>
2019-10-25 17:47:39 +0100

    Move summary statistics code to separate file

--- merge-data.R
+++ merge-data.R
@@ -11,7 +11,7 @@ old.json.files <- read.csv('json-files-2019-10-25.txt',
 old.unmatched.admission.df <- read.csv('2019-10-25-NeoTree-admission-ID-unmatched.csv',
                                        header = T,
                                        stringsAsFactors = F)
-old.final.database <- read.csv('2019-10-25-NeoTree-database.csv',
+old.final.database <- read.csv('test-final-DB.csv',
                                header = T,
                                stringsAsFactors = F)
 
@@ -19,7 +19,7 @@ old.final.database <- read.csv('2019-10-25-NeoTree-database.csv',
 library(tidyr)
 library(hms)
 library(data.table)
-library(Stack)
+#library(Stack)
 library(ggplot2)
 library(rjson)
 library(plyr)
@@ -48,6 +48,11 @@ discharge.df <- jsonToDataFrame(discharge.filenames, scriptType = "Discharge")
 admission.df <- deduplicateAdmission(admission.df)
 discharge.df <- deduplicateDischarge(discharge.df)
 
+# Check for admission matches with the 
+# discharges in the previous version
+# of the database
+#admission.df$NeoTreeID %in% old.final.database$Discharge.NeoTreeID)
+
 # Find matches 
 merged.df <- findMatchesWithinNewAdmissionDischarge(admission.df, discharge.df)
 
@@ -56,9 +61,12 @@ if (!is.null(merged.df)){ # There may be no matches in the new data
   final.database.df <- addUnmatchedDischarges(merged.df, discharge.df)
 }
 if (is.null(merged.df)){
-  final.database.df <- discharge.df
+  final.database.df <- addUnmatchedDischarges(old.final.database, 
+                             discharge.df)
 }
 
+final.database.df.v2 <- merge(final.database.df, 
+                           old.final.database)
 # Write this final database to csv
 database.file <- paste0(Sys.Date(),'-NeoTree-database.csv')
 # Check if file exists
@@ -72,7 +80,6 @@ write.csv(file=database.file,
           final.database.df, 
           row.names = F,
           quote=T)
-
 
 # Save the unmatched admissions as well   
 unmatched.admission.df <- admission.df[which(!admission.df$Admission.UID %in% 
@@ -89,44 +96,8 @@ cat(c(admission.files, discharge.files),
     sep =  '\n',
     file = paste0('json-files-', Sys.Date(), '.txt')) 
 
-
 # Summary statistics by HCWID
-# Table of admissions per week
-print("Weekly admissions by worker, including unmatched")
-weekly.admissions <- table(admission.df$Admission.MonthWeekAdmission, admission.df$Admission.HCWID)
-write.csv(weekly.admissions, file=paste0(Sys.Date(), ' NeoTree Weekly Admissions.csv'),
-          row.names = T)
+source('summary-statistics.R')
 
-print("Weekly discharges by worker")
-weekly.discharges <- table(discharge.df$Discharge.MonthWeekDischarge, discharge.df$Discharge.HCWIDDis)
-write.csv(weekly.discharges, file=paste0(Sys.Date(), ' NeoTree Weekly Discharges.csv'),
-          row.names = T)
-#discharge/death by admission month
-#Need to change Month number as needed eg 07 for July
-Month<-subset(complete.df, complete.df$Admission.MonthAdmission=="08")
-Tab<-table(Month$Discharge.HCWIDDis,Month$Discharge.NeoTreeOutcome)
-write.csv(Tab, file=paste0('DischargeMatchedByHCWID.csv'), row.names = T)
-#number of unmatched by HCWID
-#Need to change Month number e.g. 07 for July as above
-MonthUnmatched<-subset(discharge.need.match.df, discharge.need.match.df$Discharge.MonthDischarge=="08")
-tabU<-table(MonthUnmatched$Discharge.HCWIDDis,MonthUnmatched$Discharge.NeoTreeOutcome)
-write.csv(TabU, file=paste0('DischargeUnmatchedByHCWID.csv'), row.names = T)
-#attempting to merge unmatched discharges with matched file
-
-complete.df<-rbindlist(list(merged.df, discharge.need.match.df), fill = TRUE)
-write.csv(complete.df, file=paste0('Complete_WIthUnmatchedDis.csv'), row.names = T)
-tab<-table(complete.df$Discharge.NeoTreeOutcome)
-tab
-#creating final outcome variable
-complete.df$Discharge.DateTimeFinalOutcome <- ifelse(!is.na(complete.df$Discharge.DateTimeDeath), complete.df$Discharge.DateTimeDeath, ifelse(!is.na(complete.df$Discharge.DateTimeDischarge), complete.df$Discharge.DateTimeDischarge, NA))
-write.csv(complete.df, file=paste0(Sys.Date(), ' Final.csv'),
-          row.names = T)
-#duration of admission
-complete.df$DateFinal <- as.Date(gsub(pattern = "T.*", replacement = "", complete.df$Discharge.DateTimeFinalOutcome), format="%Y-%m-%d")
-complete.df$MonthFinal <- format(complete.df$DateFinal, "%m") # Month as number from 1-12
-complete.df$MonthFinal<- ordered(complete.df$MonthFinal, levels=c("11", "12", "01", "02","03","04","05","06","07","08","09","10"))
-complete.df$DaysAdmission<- (complete.df$DateFinal)-(complete.df$Admission.DateAdmission)
-summary(complete.df$DaysAdmission)
-tapply(complete.df$DaysAdmission,complete.df$Discharge.NeoTreeOutcome,summary)
-complete.df$DaysAdmission<-as.numeric(complete.df$DaysAdmission)
-table(complete.df$DaysAdmission)
+# Felicity's plotting 
+source('plot-generation.R')
---
+++ summary-statistics.R
@@ -0,0 +1,40 @@
+# Summary statistics by HCWID
+# Table of admissions per week
+print("Weekly admissions by worker, including unmatched")
+weekly.admissions <- table(admission.df$Admission.MonthWeekAdmission, admission.df$Admission.HCWID)
+write.csv(weekly.admissions, file=paste0(Sys.Date(), ' NeoTree Weekly Admissions.csv'),
+          row.names = T)
+
+print("Weekly discharges by worker")
+weekly.discharges <- table(discharge.df$Discharge.MonthWeekDischarge, discharge.df$Discharge.HCWIDDis)
+write.csv(weekly.discharges, file=paste0(Sys.Date(), ' NeoTree Weekly Discharges.csv'),
+          row.names = T)
+#discharge/death by admission month
+#Need to change Month number as needed eg 07 for July
+Month<-subset(complete.df, complete.df$Admission.MonthAdmission=="08")
+Tab<-table(Month$Discharge.HCWIDDis,Month$Discharge.NeoTreeOutcome)
+write.csv(Tab, file=paste0('DischargeMatchedByHCWID.csv'), row.names = T)
+#number of unmatched by HCWID
+#Need to change Month number e.g. 07 for July as above
+MonthUnmatched<-subset(discharge.need.match.df, discharge.need.match.df$Discharge.MonthDischarge=="08")
+tabU<-table(MonthUnmatched$Discharge.HCWIDDis,MonthUnmatched$Discharge.NeoTreeOutcome)
+write.csv(TabU, file=paste0('DischargeUnmatchedByHCWID.csv'), row.names = T)
+#attempting to merge unmatched discharges with matched file
+
+complete.df<-rbindlist(list(merged.df, discharge.need.match.df), fill = TRUE)
+write.csv(complete.df, file=paste0('Complete_WIthUnmatchedDis.csv'), row.names = T)
+tab<-table(complete.df$Discharge.NeoTreeOutcome)
+tab
+#creating final outcome variable
+complete.df$Discharge.DateTimeFinalOutcome <- ifelse(!is.na(complete.df$Discharge.DateTimeDeath), complete.df$Discharge.DateTimeDeath, ifelse(!is.na(complete.df$Discharge.DateTimeDischarge), complete.df$Discharge.DateTimeDischarge, NA))
+write.csv(complete.df, file=paste0(Sys.Date(), ' Final.csv'),
+          row.names = T)
+#duration of admission
+complete.df$DateFinal <- as.Date(gsub(pattern = "T.*", replacement = "", complete.df$Discharge.DateTimeFinalOutcome), format="%Y-%m-%d")
+complete.df$MonthFinal <- format(complete.df$DateFinal, "%m") # Month as number from 1-12
+complete.df$MonthFinal<- ordered(complete.df$MonthFinal, levels=c("11", "12", "01", "02","03","04","05","06","07","08","09","10"))
+complete.df$DaysAdmission<- (complete.df$DateFinal)-(complete.df$Admission.DateAdmission)
+summary(complete.df$DaysAdmission)
+tapply(complete.df$DaysAdmission,complete.df$Discharge.NeoTreeOutcome,summary)
+complete.df$DaysAdmission<-as.numeric(complete.df$DaysAdmission)
+table(complete.df$DaysAdmission)
